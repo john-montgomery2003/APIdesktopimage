d58d9ff729db1f40df1b50ac03dea47e4d96c669
john-montgomery2003 <johnmontgomery2003@gmail.com>
2022-03-26 16:26:38 +0000

    add ha

--- app/main.py
+++ app/main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 from mangum import Mangum
 
 app = FastAPI(title='Serverless Lambda FastAPI')
+@app.get("/")
+def main_endpoint_test():
+    return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
 
 
-@app.get("/ha")
+@app.get("/haa")
 def main_endpoint_test():
     return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
 
